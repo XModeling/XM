e08402e6dadfa51bf3207900e886781d4a03c66e
XM eXplainable Modeling <41166383+XModeling@users.noreply.github.com>
2020-03-31 12:20:06 +0200

    Update XM_HeatForecast.py

--- XM_HeatForecast/XM_HeatForecast.py
+++ XM_HeatForecast/XM_HeatForecast.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar  2 16:12:31 2020
+XM_HeatForecast - eXplainable Modeling for heat forecast
+Copyright 2020 © Alberto Castellini, Alessandro Farinelli, Federico Bianchi, Francesco Masillo
 
-@author: Federico Bianchi, Francesco Masillo, Alberto Castellini, Alessandro Farinelli
+This file is part of XM_HeatForecast.
+XM_HeatForecast is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+XM_HeatForecast is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XM_HeatForecast.  If not, see <http://www.gnu.org/licenses/>.
+
+Please, report suggestions/comments/bugs to
+ alberto.castellini@univr.it, alessandro.farinelli@univr.it, federico.bianchi@univr.it, francesco.masillo@studenti.univr.it
 """
 
 ############################################################################### 
