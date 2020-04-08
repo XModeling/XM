39e56f636169b09ee2acb6df548c5e7129f5f5c0
XModeling <xmodelingsw@gmail.com>
2020-04-08 15:41:28 +0200

    Removing old version

--- XM_HeatForecast/install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install python3-tk
-pip install --user matplotlib
-pip install --user pandas
-pip install --user numpy
-pip install --user statsmodels
-pip install --user workalendar
-pip install --user sklearn
-pip install --user apscheduler
-
